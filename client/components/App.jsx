7b9b939cab8403038d5badeaf11ef27c8c5ce9e6
Brandon Galloway <brandon.galloway2@gmail.com>
2021-07-08 16:04:04 -0400

    Added our pages to the project

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import Cats from './Cats.jsx';
+import HomePage from './HomePage.jsx';
+import AppliedPage from './AppliedPage.jsx';
+import ResponsePage from './ResponsePage.jsx';
+import ResponseSavedPage from './ResponseSavedPage.jsx';
+import LandingPage from './LandingPage.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      currentPage: 'landing-page'
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>App.js is connected and working!</h1>
-        <Cats />
       </div>
     )
   }
---
+++ client/components/ResponseSavedPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class ResponseSavedPage extends React.Component {
+    constructor(props) {
+      super(props)
+    }
+  
+    render() {
+      return (
+        <div>
+        </div>
+      )
+    }
+  }
+  
+  export default ResponseSavedPage;
